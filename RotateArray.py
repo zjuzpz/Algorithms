18f4de58b40f34bae13b0d8819fc729861ecaa69
Peizhe Zhang <zjuzpz@gmail.com>
2015-11-01 13:12:19 -0800

    Create RotateArray.py

---
+++ RotateArray.py
@@ -0,0 +1,55 @@
+"""
+189. Rotate Array
+Rotate an array of n elements to the right by k steps.
+For example, with n = 7 and k = 3, the array [1,2,3,4,5,6,7] is rotated to [5,6,7,1,2,3,4].
+
+Note:
+Try to come up as many solutions as you can, there are at least 3 different ways to solve this problem.
+"""
+class Solution(object):
+    def rotate(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: void Do not return anything, modify nums in-place instead.
+        """
+        k %= len(nums)
+        while k > 0:
+            tem = nums.pop()
+            nums.insert(0, tem)
+            k -= 1
+
+class Solution2(object):
+    def rotate(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: void Do not return anything, modify nums in-place instead.
+        """
+        k %= len(nums)
+        length = len(nums) - k
+        nums.extend(nums[0:length])
+        del(nums[0:length])
+
+class Solution3(object):
+    def rotate(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: void Do not return anything, modify nums in-place instead.
+        """
+        k %= len(nums)
+        self.reverse(nums, 0, len(nums) - 1)
+        self.reverse(nums, 0, k - 1)
+        self.reverse(nums, k, len(nums) - 1)
+        
+    def reverse(self, nums, start, end):
+        while start < end:
+            nums[start], nums[end] = nums[end], nums[start]
+            start, end = start + 1, end - 1
+
+if __name__ == "__main__":
+    nums = [1,2]
+    k = 1
+    Solution2().rotate(nums, k)
+    print(nums)
