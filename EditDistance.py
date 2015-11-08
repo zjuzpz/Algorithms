bcc45ac27554b4f17a560e033addf22168c1deb0
Peizhe Zhang <zjuzpz@gmail.com>
2015-11-08 14:03:22 -0800

    Update EditDistance.py

--- EditDistance.py
+++ EditDistance.py
@@ -19,19 +19,19 @@ class Solution(object):
         """
         if not word1 or not word2:
             return max(len(word1), len(word2))
-        res = [[float('inf') for j in range(len(word1) + 1)] for i in range(len(word2) + 1)] 
+        res = [[0 for j in range(len(word1) + 1)] for i in range(len(word2) + 1)]
         for j in range(len(word1) + 1):
             res[0][j] = j
         for i in range(len(word2) + 1):
             res[i][0] = i
         for i in range(1, len(word2) + 1):
             for j in range(1, len(word1) + 1):
-                if word2[i - 1] != word1[j - 1]:
-                    res[i][j] = min(res[i - 1][j - 1], res[i - 1][j], res[i][j - 1]) + 1
-                else:
+                if word1[j - 1] == word2[i - 1]:
                     res[i][j] = res[i - 1][j - 1]
+                else:
+                    res[i][j] = min(res[i - 1][j], res[i][j - 1], res[i - 1][j - 1]) + 1
         return res[-1][-1]
-
+    
 if __name__ == "__main__":
     word1 = "zoologicoarchaeologist"
     word2 = "zoogeologist" 
