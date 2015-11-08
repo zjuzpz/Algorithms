dde4631bfcbd1ec049978868992e7968fcd98868
Peizhe Zhang <zjuzpz@gmail.com>
2015-11-08 11:45:14 -0800

    Create PathSumII.py

---
+++ PathSumII.py
@@ -0,0 +1,62 @@
+"""
+113. Path Sum II
+Given a binary tree and a sum, find all root-to-leaf paths where each path's sum equals the given sum.
+
+For example:
+Given the below binary tree and sum = 22,
+              5
+             / \
+            4   8
+           /   / \
+          11  13  4
+         /  \    / \
+        7    2  5   1
+return
+[
+   [5,4,11,2],
+   [5,8,4,5]
+]
+"""
+# O(n)
+# O(h)
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution(object):
+    def pathSum(self, root, sum):
+        """
+        :type root: TreeNode
+        :type sum: int
+        :rtype: List[List[int]]
+        """
+        if not root:
+            return []
+        res = []
+        self.recur(res, [], root, sum)
+        return res
+        
+    def recur(self, res, cur, root, target):
+        if not root.left and not root.right:
+            if root.val == target:
+                res.append(cur + [root.val])
+            return
+        if root.left:
+            self.recur(res, cur + [root.val], root.left, target - root.val)
+        if root.right:
+            self.recur(res, cur + [root.val], root.right, target - root.val)
+
+if __name__ == "__main__":
+    root = TreeNode(5)
+    root.left = TreeNode(4)
+    root.right = TreeNode(8)
+    root.left.left = TreeNode(11)
+    root.left.left.left = TreeNode(7)
+    root.left.left.right = TreeNode(2)
+    root.right.left = TreeNode(13)
+    root.right.right = TreeNode(4)
+    root.right.right.left = TreeNode(5)
+    root.right.right.right = TreeNode(1)
+    print(Solution().pathSum(root,22))
