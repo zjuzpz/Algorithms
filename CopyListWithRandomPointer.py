be829ddefeed99c7d0dd34b1686fcff21c11f4f1
Peizhe Zhang <zjuzpz@gmail.com>
2015-11-12 23:01:32 -0800

    Update CopyListWithRandomPointer.py

--- CopyListWithRandomPointer.py
+++ CopyListWithRandomPointer.py
@@ -1,5 +1,8 @@
 """
 138. Copy List with Random Pointer
+A linked list is given such that each node contains an additional random pointer which could point to any node in the list or null.
+
+Return a deep copy of the list.
 """
 class RandomListNode:
     def __init__(self, x):
