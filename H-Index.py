8f5f64990f502e5cd63bb78058a95378ee8c10fe
Peizhe Zhang <zjuzpz@gmail.com>
2015-11-18 13:03:27 -0800

    Create H-Index.py

---
+++ H-Index.py
@@ -0,0 +1,32 @@
+"""
+274. H-Index
+Given an array of citations (each citation is a non-negative integer) of a researcher, write a function to compute the researcher's h-index.
+
+According to the definition of h-index on Wikipedia: "A scientist has index h if h of his/her N papers have at least h citations each, and the other N − h papers have no more than h citations each."
+
+For example, given citations = [3, 0, 6, 1, 5], which means the researcher has 5 papers in total and each of them had received 3, 0, 6, 1, 5 citations respectively. Since the researcher has 3 papers with at least 3 citations each and the remaining two with no more than 3 citations each, his h-index is 3.
+
+Note: If there are several possible values for h, the maximum one is taken as the h-index.
+"""
+# O(n)
+# O(n)
+class Solution(object):
+    def hIndex(self, citations):
+        """
+        :type citations: List[int]
+        :rtype: int
+        """
+        lookup = [0 for i in range(len(citations) + 1)]
+        for i in range(len(citations)):
+            if citations[i] > len(citations):
+                lookup[-1] += 1
+            else:
+                lookup[citations[i]] += 1
+        count = 0
+        for i in reversed(range(len(lookup))):
+            count += lookup[i]
+            if count >= i:
+                return i
+        return count
+if __name__ == "__main__":
+    print(Solution().hIndex([3, 0, 6, 1, 5]))
