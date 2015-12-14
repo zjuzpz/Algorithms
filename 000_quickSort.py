7905b02e8a16c2a0da11d6aebd80362c5a03158f
Peizhe Zhang <zjuzpz@gmail.com>
2015-12-13 17:21:00 -0800

    Create 000_quickSort.py

---
+++ 000_quickSort.py
@@ -0,0 +1,20 @@
+# O(nlogn)
+# O(nlogn)
+def quickSort(nums, start, end):
+    if start >= end:
+        return nums
+    i, j, key = start, end, nums[start]
+    while i < j:
+        while i < j and nums[j] >= key:
+            j -= 1
+        nums[i], nums[j] = nums[j], nums[i]
+        while i < j and nums[i] <= key:
+            i += 1
+        nums[i], nums[j] = nums[j], nums[i]
+    quickSort(nums, start, i - 1)
+    quickSort(nums, i + 1, end)
+
+if __name__ == "__main__":
+    nums = [2, 4, 5, 1, 0, 9, 8, 8, 2, 3, 11, -3, 5]
+    quickSort(nums, 0, len(nums) - 1)
+    print(nums)
