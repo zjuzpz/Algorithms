2d970d61e08061b32aab53adf929dfdf9480b909
Peizhe Zhang <zjuzpz@gmail.com>
2015-12-18 11:53:29 -0800

    Create IntegerToEnglishWords.py

---
+++ IntegerToEnglishWords.py
@@ -0,0 +1,56 @@
+"""
+273. Integer to English Words
+Convert a non-negative integer to its english words representation. Given input is guaranteed to be less than 231 - 1.
+
+For example,
+123 -> "One Hundred Twenty Three"
+12345 -> "Twelve Thousand Three Hundred Forty Five"
+1234567 -> "One Million Two Hundred Thirty Four Thousand Five Hundred Sixty Seven"
+"""
+# O(logn)
+# O(1)
+class Solution(object):
+    def numberToWords(self, num):
+        """
+        :type num: int
+        :rtype: str
+        """
+        if num == 0:
+            return "Zero"
+        lookup = {1:"One", 2:"Two", 3:"Three", 4:"Four", 5:"Five", 6:"Six", 7:"Seven", 8:"Eight", 9:"Nine", 10:"Ten",\
+        11:"Eleven", 12:"Twelve", 13:"Thirteen", 14:"Fourteen", 15:"Fifteen", 16:"Sixteen", 17:"Seventeen", 18:"Eighteen",\
+        19:"Nineteen", 20:"Twenty", 30:"Thirty", 40:"Forty", 50:"Fifty", 60:"Sixty", 70:"Seventy", 80:"Eighty", 90:"Ninety"}
+        res = []
+        i = num // 1000000000
+        if i > 0:
+            res.append(lookup[i])
+            res.append("Billion")
+            num %= 1000000000
+        i = num // 1000000
+        if i > 0:
+            self.helper(res, i, lookup)
+            res.append("Million")
+            num %= 1000000
+        i = num // 1000
+        if i > 0:
+            self.helper(res, i, lookup)
+            res.append("Thousand")
+            num %= 1000
+        if num > 0:
+            self.helper(res, num, lookup)
+        return " ".join(res)
+        
+    def helper(self, res, num, lookup):
+        i = num // 100
+        if i > 0:
+            res.append(lookup[i])
+            res.append("Hundred")
+            num %= 100
+        if num in lookup:
+            res.append(lookup[num])
+        elif num > 0:
+            res.append(lookup[num // 10 * 10])
+            res.append(lookup[num % 10])
+
+if __name__ == "__main__":
+    print(Solution().numberToWords(1234567890))
