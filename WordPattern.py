e973eef2dbb564db057ee3f3f5e1c06096f8d087
Peizhe Zhang <zjuzpz@gmail.com>
2016-01-06 19:26:51 -0800

    Update WordPattern.py

--- WordPattern.py
+++ WordPattern.py
@@ -14,7 +14,7 @@ Notes:
 You may assume pattern contains only lowercase letters, and str contains lowercase letters separated by a single space.
 """
 # O(n)
-# O(n)
+# O(1) It is because from a to z, the maximum possible distinct pattern we have is 26 
 class Solution(object):
     def wordPattern(self, pattern, str):
         """
@@ -37,39 +37,6 @@ class Solution(object):
                     return False
         return True
 
-# O(n)
-# O(1)
-class Solution2(object):
-    def wordPattern(self, pattern, str):
-        """
-        :type pattern: str
-        :type str: str
-        :rtype: bool
-        """
-        lookup1, lookup2, count = {}, {}, 0
-        i, j = 0, 0
-        while i < len(str):
-            left = i
-            while i < len(str) and str[i] != " ":
-                i += 1
-            word = str[left: i]
-            if j >= len(pattern):
-                return False
-            if pattern[j] not in lookup1:
-                lookup1[pattern[j]] = word
-            elif lookup1[pattern[j]] != word:
-                return False
-            if word not in lookup2:
-                count += 1
-                if count > 26:
-                    return False
-                lookup2[word] = pattern[j]
-            elif lookup2[word] != pattern[j]:
-                return False
-            i, j = i + 1, j + 1
-        if j == len(pattern):
-            return True
-        return False
 
 if __name__ == "__main__":
-    print(Solution2().wordPattern("abba", "cat dog dog cat"))
+    print(Solution().wordPattern("abba", "cat dog dog cat"))
