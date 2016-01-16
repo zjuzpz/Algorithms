e9a28ac57f15d29915e99d3cc25c6169f7f3c036
Peizhe Zhang <zjuzpz@gmail.com>
2016-01-16 11:13:45 -0800

    Update SpiralMatrix.py

--- SpiralMatrix.py
+++ SpiralMatrix.py
@@ -38,6 +38,31 @@ class Solution(object):
             top, right, bottom, left = top + 1, right - 1, bottom - 1, left + 1
         return res
 
+class Solution2(object):
+    def spiralOrder(self, matrix):
+        """
+        :type matrix: List[List[int]]
+        :rtype: List[int]
+        """
+        if not matrix:
+            return []
+        m, n = len(matrix), len(matrix[0])
+        times = (min(m, n) + 1) // 2
+        i, res = 0, []
+        while i < times:
+            for j in range(i, n - i):
+                res.append(matrix[i][j])
+            for j in range(i + 1, m - i - 1):
+                res.append(matrix[j][n - i - 1])
+            if m - 1 - i != i:
+                for j in reversed(range(i, n - i)):
+                    res.append(matrix[m - i - 1][j])
+            if n - 1 - i != i:
+                for j in reversed(range(i + 1, m - i - 1)):
+                    res.append(matrix[j][i])
+            i += 1
+        return res
+
 if __name__ == "__main__":
     matrix = [[1,2,3],[4,5,6],[7,8,9]]
-    print(Solution().spiralOrder(matrix))
+    print(Solution2().spiralOrder(matrix))
