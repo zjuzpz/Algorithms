af5ce9121c6e6df0bc4e8d62e76243206a114fc8
Peizhe Zhang <zjuzpz@gmail.com>
2016-01-21 20:15:10 -0800

    Update StrobogrammaticNumberII.py

--- StrobogrammaticNumberII.py
+++ StrobogrammaticNumberII.py
@@ -15,45 +15,44 @@ class Solution(object):
         :type n: int
         :rtype: List[str]
         """
+        if n <= 0:
+            return []
         res = []
         if n % 2 == 1:
-            mid = True
+            self.find(res, [], n // 2, True)
         else:
-            mid = False
-        self.generate(res, [], n // 2, mid)
+            self.find(res, [], n // 2, False)
         return res
         
-    def generate(self, res, cur, length, mid):
+    def find(self, res, cur, length, mid):
         if len(cur) == length:
-            res.extend(self.helper(cur, mid))
-            return
-        if len(cur) == 0:
-            for i in ("1", "6", "8", "9"):
-                cur.append(i)
-                self.generate(res, cur, length, mid)
-                cur.pop()
-        else:
-            for i in ("0", "1", "6", "8", "9"):
-                cur.append(i)
-                self.generate(res, cur, length, mid)
-                cur.pop()
-                
-    def helper(self, left, mid):
-        res, right = [], []
-        for i in reversed(range(len(left))):
-            if left[i] in ("0", "1", "8"):
-                right.append(left[i])
-            elif left[i] == "6":
-                right.append("9")
+            right = self.getRight(cur)
+            if mid:
+                res.append("".join(cur + ["0"] + right))
+                res.append("".join(cur + ["1"] + right))
+                res.append("".join(cur + ["8"] + right))
             else:
-                right.append("6")
-        if mid:
-            for i in ("0", "1", "8"):
-                tem = left + [i] + right
-                res.append("".join(tem))
-        else:
-            tem = left + right
-            res.append("".join(tem))
+                res.append("".join(cur + right))
+            return
+        if len(cur) != 0:
+            cur.append("0")
+            self.find(res, cur, length, mid)
+            cur.pop()
+        for i in ["1", "6", "8", "9"]:
+            cur.append(i)
+            self.find(res, cur, length, mid)
+            cur.pop()
+            
+    def getRight(self, l):
+        res = []
+        for i in l:
+            if i in ["0", "1", "8"]:
+                res.append(i)
+            elif i == "6":
+                res.append("9")
+            else:
+                res.append("6")
+        res.reverse()
         return res
 
 if __name__ == "__main__":
