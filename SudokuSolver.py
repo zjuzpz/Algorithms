303a24a110aa169f20ae7b971b356e8744fde2de
Peizhe Zhang <zjuzpz@gmail.com>
2016-01-23 21:13:27 -0800

    Update SudokuSolver.py

--- SudokuSolver.py
+++ SudokuSolver.py
@@ -4,6 +4,8 @@ Write a program to solve a Sudoku puzzle by filling the empty cells.
 Empty cells are indicated by the character '.'.
 You may assume that there will be only one unique solution.
 """
+# O((9!) ^ 9)
+# O(1)
 class Solution(object):
     def solveSudoku(self, board):
         """
@@ -28,8 +30,44 @@ class Solution(object):
         for i in range(9):
             if i != x and board[i][y] == board[x][y]:
                 return False
-        for j in range(9):
-            if j != y and board[x][j] == board[x][y]:
+            if i != y and board[x][i] == board[x][y]:
+                return False
+        for i in range(x // 3 * 3, x // 3 * 3 + 3):
+            for j in range(y // 3 * 3, y // 3 * 3 + 3):
+                if (i, j) != (x, y) and board[i][j] == board[x][y]:
+                    return False
+        return True
+
+class Solution2(object):
+    def solveSudoku(self, board):
+        """
+        :type board: List[List[str]]
+        :rtype: void Do not return anything, modify board in-place instead.
+        """
+        stack = []
+        for i in range(9):
+            for j in range(9):
+                if board[i][j] == ".":
+                    stack.append((i, j))
+        self.recur(board, stack)
+        
+    def recur(self, board, stack):
+        if not stack:
+            return True
+        (x, y) = stack.pop()
+        for i in range(1, 10):
+            board[x][y] = str(i)
+            if self.isValid(board, x, y) and self.recur(board, stack):
+                return True
+        board[x][y] = "."
+        stack.append((x, y))
+        return False
+    
+    def isValid(self, board, x, y):
+        for i in range(9):
+            if i != x and board[i][y] == board[x][y]:
+                return False
+            if i != y and board[x][i] == board[x][y]:
                 return False
         for i in range(x // 3 * 3, x // 3 * 3 + 3):
             for j in range(y // 3 * 3, y // 3 * 3 + 3):
