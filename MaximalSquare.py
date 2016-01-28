9b0aeebeaacff2b320d747126fbc9425a4fc8e6b
Peizhe Zhang <zjuzpz@gmail.com>
2016-01-27 21:46:20 -0800

    Update MaximalSquare.py

--- MaximalSquare.py
+++ MaximalSquare.py
@@ -9,37 +9,32 @@ For example, given the following matrix:
 1 0 0 1 0
 Return 4.
 """
-# O(n * m)
-# O(1)
+# O(m * n)
+# O(m * n)
 class Solution(object):
     def maximalSquare(self, matrix):
         """
         :type matrix: List[List[str]]
         :rtype: int
         """
-        res = 0
         if not matrix:
-            return res
+            return 0
+        a = 0
+        res = [[0 for j in range(len(matrix[0]))] for i in range(len(matrix))]
         for i in range(len(matrix)):
             if matrix[i][0] == "1":
-                res = 1
-                matrix[i][0] = 1
-            else:
-                matrix[i][0] = 0
-        for j in range(1, len(matrix[0])):
+                res[i][0] = 1
+                a = 1
+        for j in range(len(matrix[0])):
             if matrix[0][j] == "1":
-                res = 1
-                matrix[0][j] = 1
-            else:
-                matrix[0][j] = 0
+                res[0][j] = 1
+                a = 1
         for i in range(1, len(matrix)):
             for j in range(1, len(matrix[0])):
-                if matrix[i][j] == "0":
-                    matrix[i][j] = 0
-                else:
-                    matrix[i][j] = min(matrix[i - 1][j], matrix[i - 1][j - 1], matrix[i][j - 1]) + 1
-                    res = max(res, matrix[i][j])
-        return res * res
+                if matrix[i][j] == "1":
+                    res[i][j] = min(res[i - 1][j], res[i][j - 1], res[i - 1][j - 1]) + 1
+                    a = max(a, res[i][j])
+        return a ** 2
 
 
 if __name__ == "__main__":
