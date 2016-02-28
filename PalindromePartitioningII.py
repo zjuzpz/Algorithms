1e4f6e7176d1e39494aa5e057f5b7a10f670a440
Peizhe Zhang <zjuzpz@gmail.com>
2016-02-28 12:55:14 -0800

    Update PalindromePartitioningII.py

--- PalindromePartitioningII.py
+++ PalindromePartitioningII.py
@@ -46,45 +46,23 @@ class Solution2(object):
         :type s: str
         :rtype: int
         """
-        lookup = [[True for j in s] for i in s]
-        for i in range(1, len(s)):
-            for j in range(i):
-                if s[j] != s[i]:
-                    lookup[i][j] = False
-                else:
-                    lookup[i][j] = lookup[i - 1][j + 1]
-        res = [float("inf") for i in range(len(s))]
-        for i in range(len(s)):
-            if lookup[i][0]:
-                res[i] = 0
-            else:
-                for j in range(i):
-                    if lookup[i][j + 1]:
-                        res[i] = min(res[i], res[j] + 1)
-        return res[-1]
-
-class Solution3(object):
-    def minCut(self, s):
-        """
-        :type s: str
-        :rtype: int
-        """
         lookup = [[False for j in s] for i in s]
         for i in reversed(range(len(s))):
-            lookup[i][i] = True
-            for j in range(i + 1, len(s)):
-                if s[i] == s[j] and (j - i <= 2 or lookup[i + 1][j - 1]):
+            for j in range(len(s)):
+                if i >= j:
                     lookup[i][j] = True
-        res = [float("inf") for i in range(len(s))]
+                elif s[i] == s[j] and lookup[i + 1][j - 1]:
+                    lookup[i][j] = True
+        res = [float("inf") for i in s]
         for i in range(len(s)):
             if lookup[0][i]:
                 res[i] = 0
             else:
-                for j in range(i):
-                    if lookup[j + 1][i]:
-                        res[i] = min(res[i], res[j] + 1)
+                for j in range(1, i + 1):
+                    if lookup[j][i]:
+                        res[i] = min(res[i], res[j - 1] + 1)
         return res[-1]
     
 if __name__ == "__main__":
     s = "abbab"
-    print(Solution3().minCut(s))
+    print(Solution2().minCut(s))
