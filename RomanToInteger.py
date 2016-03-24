37d56afe1e505086612e6090ecdca04cd58ddbb8
Peizhe Zhang <zjuzpz@gmail.com>
2016-03-23 20:47:12 -0700

    Update RomanToInteger.py

--- RomanToInteger.py
+++ RomanToInteger.py
@@ -11,13 +11,12 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        lookup = {"I":1, "V":5, "X":10, "L":50, "C":100, "D":500, "M":1000}
+        lookup = {"I":1, "II":2, "III":3, "V":5, "X":10, "L":50, "C":100, "D":500, "M":1000}
         res = 0
         for i in range(len(s)):
-            if i < len(s) - 1 and lookup[s[i]] < lookup[s[i + 1]]:
-                res -= lookup[s[i]]
-            else:
-                res += lookup[s[i]]
+            res += lookup[s[i]]
+            if i and lookup[s[i]] > lookup[s[i - 1]]:
+                res -= 2 * lookup[s[i - 1]]
         return res
 
 if __name__ == "__main__":
