f2ff2409247fa92d8620bed0b3c0ae946678b667
Peizhe Zhang <zjuzpz@gmail.com>
2016-04-28 17:50:01 -0700

    Update UniquePathsII.py

--- UniquePathsII.py
+++ UniquePathsII.py
@@ -5,51 +5,26 @@ Now consider if some obstacles are added to the grids. How many unique paths wou
 An obstacle and empty space is marked as 1 and 0 respectively in the grid.
 """
 # O(m * n)
-# O(m * n)
+# O(n)
 class Solution(object):
     def uniquePathsWithObstacles(self, obstacleGrid):
         """
         :type obstacleGrid: List[List[int]]
         :rtype: int
         """
-        g = obstacleGrid
-        if not g or g[0][0] == 1:
+        if not obstacleGrid:
             return 0
-        m, n = len(g), len(g[0])
-        g[0][0] = 1
-        i, j = 1, 1
-        while j < n:
-            if g[0][j] == 0:
-                g[0][j] = 1
-            else:
-                while j < n:
-                    g[0][j] = 'X'
-                    j += 1
-            j += 1
-        while i < m:
-            if g[i][0] == 0:
-                g[i][0] = 1
-            else:
-                while i < m:
-                    g[i][0] = 'X'
-                    i += 1
-            i += 1
+        m, n = len(obstacleGrid), len(obstacleGrid[0])
+        ways = [0 for i in range(n)]
+        ways[0] = 1 if obstacleGrid[0][0] == 0 else 0
+        for j in range(1, n):
+            ways[j] = ways[j - 1] if obstacleGrid[0][j] == 0 else 0
         for i in range(1, m):
+            if obstacleGrid[i][0] == 1:
+                ways[0] = 0
             for j in range(1, n):
-                if g[i][j] == 0:
-                    if g[i - 1][j] == g[i][j - 1] == 'X':
-                        g[i][j] = 'X'
-                    elif g[i - 1][j] == 'X':
-                        g[i][j] = g[i][j - 1]
-                    elif g[i][j - 1] == 'X':
-                        g[i][j] = g[i - 1][j]
-                    else:
-                        g[i][j] = g[i - 1][j] + g[i][j - 1]
-                else:
-                    g[i][j] = 'X'
-        if g[-1][-1] == 'X':
-            return 0
-        return g[-1][-1]
+                ways[j] = ways[j] + ways[j - 1] if obstacleGrid[i][j] == 0 else 0
+        return ways[-1]
 
 if __name__ == "__main__":
     obstacleGrid = [[0,0,0],[0,1,0],[0,0,0]]
