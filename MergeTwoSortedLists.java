089357b2148b0ce75640578d391b3809d3f1837a
Peizhe Zhang <zjuzpz@gmail.com>
2016-07-11 20:34:19 -0700

    Create MergeTwoSortedLists.java

---
+++ MergeTwoSortedLists.java
@@ -0,0 +1,25 @@
+
+public class MergeTwoSortedLists {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(-1);
+        ListNode cur = dummy;
+        while(l1 != null && l2 != null) {
+            if(l1.val <= l2.val) {
+                cur.next = l1;
+                l1 = l1.next;
+            } else {
+                cur.next = l2;
+                l2 = l2.next;
+            }
+            cur = cur.next;
+        }
+        if(l1 != null) {
+            cur.next = l1;
+        }
+        if(l2 != null) {
+            cur.next = l2;
+        }
+        return dummy.next;
+    }
+
+}
