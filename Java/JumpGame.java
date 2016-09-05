1c846a9940ba6582d941bbb98ef738de85c4d8be
Peizhe Zhang <zjuzpz@gmail.com>
2016-09-05 13:49:24 -0700

    Create JumpGame.java

---
+++ Java/JumpGame.java
@@ -0,0 +1,15 @@
+
+public class JumpGame {
+    public boolean canJump(int[] nums) {
+        int maxReach = 0;
+        for(int i = 0; i < nums.length; i++) {
+            if(i > maxReach) {
+                return false;
+            } else {
+                maxReach = Math.max(maxReach, i + nums[i]);
+            }
+        }
+        return true;
+    }
+
+}
